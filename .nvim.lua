a6094ba536ebcbbd9b0433d707c7178250047a21
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-10-02 19:50:27 +0530

    feat: add project nvim indent config

---
+++ .nvim.lua
@@ -0,0 +1,7 @@
+vim.o.shiftwidth = 4     -- Indent width
+vim.o.tabstop = 4        -- Tab width
+vim.o.softtabstop = 4    -- Soft tab stop
+vim.o.expandtab = true   -- Use spaces instead of tabs
+vim.o.smartindent = true -- Auto-indent new lines
+vim.o.autoindent = true  -- Copy indent from previous line
+vim.o.shiftround = true  -- Round indent to nearest multiple of shiftwidth
