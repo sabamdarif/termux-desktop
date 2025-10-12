f05a44568f1ee71c457373046f3907e9f260de54
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-10-12 22:03:02 +0530

    swap: use .editorconfig to add project indent related settings

--- .nvim.lua
+++
@@ -1,7 +0,0 @@
-vim.o.shiftwidth = 4     -- Indent width
-vim.o.tabstop = 4        -- Tab width
-vim.o.softtabstop = 4    -- Soft tab stop
-vim.o.expandtab = true   -- Use spaces instead of tabs
-vim.o.smartindent = true -- Auto-indent new lines
-vim.o.autoindent = true  -- Copy indent from previous line
-vim.o.shiftround = true  -- Round indent to nearest multiple of shiftwidth
